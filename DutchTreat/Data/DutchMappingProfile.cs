c5b1eb9a0c4bf77580a4395e271f1e1dfd69858e
Roland Papdi <papdiroland@hotmail.com>
2022-02-12 23:12:49 +0100

    Association Controllers

--- DutchTreat/Data/DutchMappingProfile.cs
+++ DutchTreat/Data/DutchMappingProfile.cs
@@ -10,6 +10,9 @@ namespace DutchTreat.Data
             CreateMap<Order, OrderViewModel>()
                 .ForMember(o => o.OrderId, ex => ex.MapFrom(o => o.Id))
                 .ReverseMap();
+
+            CreateMap<OrderItem, OrderItemViewModel>()
+                .ReverseMap();
         }
     }
 }
